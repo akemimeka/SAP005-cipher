395b5963f7f528ed8a3a80270c4bf4e7e6f3049e
akemimeka <akemimeka@gmail.com>
2020-10-28 12:23:48 -0300

    Cria array do alfabeto em cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,7 @@
 const cipher = {
 
   encode() {
-
-
+    const originalAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   }
 
 };
