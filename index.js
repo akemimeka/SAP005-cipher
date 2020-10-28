b41569b8e451960289bcc006dc9bb15d1f74f2a0
akemimeka <akemimeka@gmail.com>
2020-10-28 11:51:22 -0300

    Conecta cipher.js e index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// import cipher from './cipher.js';
+import cipher from './cipher.js';
 
 const originalMessage = document.getElementById('original-message');
 const offsetNumber = document.getElementById('offset-number');
 const encryptButton = document.getElementById('encrypt-button');
 const decryptButton = document.getElementById('decrypt-button');
-encryptButton.addEventListener('click', Encode);
 
-function Encode(event) {
+encryptButton.addEventListener('click', event => {
   event.preventDefault();
-  console.log(`${originalMessage.value}, ${offsetNumber.value}`)
-}
+  cipher.encode();
+});
