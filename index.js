4fbf16327dcf5c28e496b2d32365b6e4fb6e0e8b
akemimeka <akemimeka@gmail.com>
2020-10-28 11:10:38 -0300

    Cria teste com console.log para ver se a captura dos valores dos inputs está funcionando

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-import cipher from './cipher.js';
+// import cipher from './cipher.js';
 
-const originalMessage = document.getElementById("original-message").value;
-const offsetNumber = document.getElementById('number').value;
+const originalMessage = document.getElementById('original-message');
+const offsetNumber = document.getElementById('offset-number');
 const encryptButton = document.getElementById('encrypt-button');
 const decryptButton = document.getElementById('decrypt-button');
+encryptButton.addEventListener('click', Encode);
+
+function Encode(event) {
+  event.preventDefault();
+  console.log(`${originalMessage.value}, ${offsetNumber.value}`)
+}
