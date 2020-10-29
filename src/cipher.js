0672d834f42013c4a6d3267f3190a1773b63e2fa
akemimeka <akemimeka@gmail.com>
2020-10-29 20:25:38 -0300

    Insere o corpo da função encode(), que codifica a mensagem

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,25 @@
+// const originalAlphabet = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8, J: 8, K: 9, L: 10, M: 11, N: 12, O: 13, P: 14, Q: 15, R: 16, S: 17, T: 18, U: 19, V: 20, W: 21, X: 22, Y: 23, Z: 24 };
+
+// const originalAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 const cipher = {
 
-  encode() {
-    const originalAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  encode: (originalMessage, offsetNumber) => {
+    let finalMessage = "";
+
+    for (const letter of originalMessage) {
+      const letterCharCode = letter.charCodeAt();
+      const letterRealNumber = letterCharCode - 65;
+      const alphabetLetters = 26;
+      const newLetterCharCode = ((letterRealNumber + offsetNumber) % alphabetLetters) + 65;
+      const encryptedLetter = String.fromCharCode(newLetterCharCode);
+      
+      finalMessage += encryptedLetter;
+    }
+      console.log(finalMessage)
+      return finalMessage;
   }
+
 
 };
 
