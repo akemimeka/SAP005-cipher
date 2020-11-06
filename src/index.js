abd8b781eee5304919493c2693a47bdbc43f34a3
akemimeka <akemimeka@gmail.com>
2020-11-06 17:45:30 -0300

    Remove conversão para letras maiúsculas do input da mensagem

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import cipher from './cipher.js';
 
-const getOriginalMessage = document.getElementById('original-message');
-const getOffsetNumber = document.getElementById('offset-number');
-const getTranslatedMessage = document.getElementById('translated-message');
-const encryptButton = document.getElementById('encrypt-button');
-const decryptButton = document.getElementById('decrypt-button');
+const getOriginalMessage = document.getElementById("original-message");
+const getOffsetNumber = document.getElementById("offset-number");
+const getTranslatedMessage = document.getElementById("translated-message");
+const encryptButton = document.getElementById("encrypt-button");
+const decryptButton = document.getElementById("decrypt-button");
 
-encryptButton.addEventListener('click', event => {
+encryptButton.addEventListener("click", event => {
   event.preventDefault();
-  const originalMessage = String(getOriginalMessage.value).toUpperCase();
+  const originalMessage = getOriginalMessage.value;
   const offsetNumber = Number(getOffsetNumber.value);
 
   const encryptedMessage = cipher.encode(offsetNumber, originalMessage);
   getTranslatedMessage.innerHTML = encryptedMessage;
 });
 
-decryptButton.addEventListener('click', event => {
+decryptButton.addEventListener("click", event => {
   event.preventDefault();
-  const originalMessage = String(getOriginalMessage.value).toUpperCase();
+  const originalMessage = getOriginalMessage.value;
   const offsetNumber = Number(getOffsetNumber.value);
 
   const decryptedMessage = cipher.decode(offsetNumber, originalMessage);
